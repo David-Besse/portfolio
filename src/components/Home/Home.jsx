d6972e0692d51633d9afdb8d5cd47180360f1c21
David Besse <dahodprod@gmail.com>
2023-06-06 01:21:10 +0200

    'homepage done..for now'

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,57 +1,12 @@
-import { useEffect, useState } from "react";
-import { gsap } from "gsap";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-
 import { BsDownload, BsLinkedin, BsGithub } from "react-icons/bs";
 
 import "./home.scss";
 import Logo from "/logo.png";
 
-gsap.registerPlugin(MotionPathPlugin);
-
 const Home = () => {
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    };
-
-    window.addEventListener("resize", handleResize);
-  }, []);
-
-  useEffect(() => {
-    const containerWidth =
-      document.querySelector(".surfer-container").offsetWidth;
-    const surferWidth = document.querySelector(".surfer").offsetWidth;
-
-    gsap
-      .timeline({ repeat: -1 })
-      .fromTo(
-        ".surfer",
-        { x: 0 },
-        {
-          x: containerWidth - surferWidth,
-          duration: 2,
-          ease: "power1.inOut",
-        }
-      )
-      .to(".surfer", {
-        x: 0,
-        duration: 2,
-        ease: "power1.inOut",
-      });
-  }, [windowSize.width]);
-
   return (
     <div id="homePage" className="homePage h-screen snap-center flex-col">
-      <header className="h-1/6 p-4 w-screen lg:w-4/5 lg:mx-auto bg-white flex justify-between border-b-2">
+      <header className="h-1/6 p-4 w-screen md:w-4/5 md:mx-auto bg-white flex justify-between border-b-2">
         <img src={Logo} alt="logo" className="myLogo rounded-xl w-30 h-30" />
         <ul className="flex items-center justify-center gap-8">
           <li className="myCv w-22 h-16 flex justify-center items-center">
@@ -85,9 +40,9 @@ const Home = () => {
           </li>
         </ul>
       </header>
-      <section className="h-5/6 flex flex-col lg:flex-row">
-        <div className="left w-screen lg:w-2/5 h-1/3 lg:h-full flex flex-col justify-center items-center p-4 gap-4 lg:gap-10">
-          <h2 className="text-6xl lg:text-8xl font-bold">My motto</h2>
+      <section className="h-5/6 flex flex-col md:flex-row md:pl-14 xl:pl-24 md:pr-0">
+        <div className="left w-screen md:w-2/5 h-1/3 md:h-full flex flex-col justify-center items-center px-10 md:px-0 gap-4 md:gap-10">
+          <h2 className="text-6xl md:text-7xl lg:text-8xl font-bold">My motto</h2>
           <q className="text-xl lg:text-2xl italic text-justify">
             For the things we have to learn before we can do them, we learn by
             doing them.
@@ -103,10 +58,8 @@ const Home = () => {
             </a>
           </cite>
         </div>
-        <div className="right w-screen lg:w-3/5 h-2/3 lg:h-full flex justify-center items-start lg:items-center">
-          <div className="surfer-container w-4/5 h-3/5 border">
-            <div className="surfer"></div>
-          </div>
+        <div className="right w-screen md:w-3/5 md:h-full flex justify-center items-center px-10 md:px-0">
+          <img src="/4880440.jpg" alt="ecran d'ordinateur" className="pcScreen w-full xl:w-5/6" />
         </div>
       </section>
     </div>
