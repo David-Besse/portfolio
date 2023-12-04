8ba9a126b114bf1c3a2eae626dc429e902102fe1
David Besse <dahodprod@gmail.com>
2023-12-04 22:17:27 +0100

    'REACT: strict mode disabled'

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import { passiveSupport } from "passive-events-support/src/utils";
-passiveSupport({events:['wheel']});
+passiveSupport({ events: ["wheel"] });
 
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
 import App from "./components/App/App";
 import "./styles.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
-  </React.StrictMode>
+  <NextUIProvider>
+    <App />
+  </NextUIProvider>
 );
