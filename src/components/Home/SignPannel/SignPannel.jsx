17d3e819ffc47d6ddba2f1193eb82d552e02edfb
David Besse <dahodprod@gmail.com>
2023-12-16 10:32:31 +0100

    'CSS: aboutMe page, hide SignPannel for tiny screen'

--- src/components/Home/SignPannel/SignPannel.jsx
+++ src/components/Home/SignPannel/SignPannel.jsx
@@ -1,11 +1,13 @@
 import gsap from "gsap";
 import { useEffect, useRef } from "react";
 import "./SignPannel.scss";
+import { useMediaQuery } from "@uidotdev/usehooks";
 
 const arrowsPanel = Array.from(">>>");
 
 const SignPannel = () => {
   const panelContainerRef = useRef(null);
+  const isTinyScreen = useMediaQuery("(max-width: 424px)");
 
   useEffect(() => {
     const panelContainerRefElement = Array.from(
@@ -29,7 +31,7 @@ const SignPannel = () => {
   return (
     <div
       ref={panelContainerRef}
-      className="panelContainer z-10 text-4xl sm:text-6xl w-fit h-fit p-2 text-white dark:text-[#4d4d4d]"
+      className={`${isTinyScreen ? "hidden" : "block"} panelContainer z-10 text-xl sm:text-4xl lg:text-5xl w-fit h-fit p-2 text-white dark:text-[#4d4d4d]`}
     >
       {arrowsPanel.map((arrow, i) => (
         <span key={`${arrow}_${i}`}>{arrow}</span>
